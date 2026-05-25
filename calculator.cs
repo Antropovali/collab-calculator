215c3e70643907bc0dae87618a1da37be17296f3
Liza <antropova0606@mail.ru>
2026-05-25 09:10:58 +0300

    Добавлен вывод умножения

--- calculator.cs
+++ calculator.cs
@@ -5,3 +5,4 @@ class Calc {
         int a = 5, b = 3;
         Console.WriteLine($"{a}+{b}={Add(a, b)}");
         Console.WriteLine($"{a}-{b}={Sub(a,b)}");
+        Console.WriteLine($"{a}*{b}={Mul(a, b)}");
